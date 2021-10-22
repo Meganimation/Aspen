07d68d8a2c68cc9f45135b594fb6624b3a67f4ec
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2021-10-22 14:52:48 -0400

    implemented starbucks data and mapped on topology

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
-
-const position = [51.505, -0.09]
-
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const position = [45.514242, -122.683175];
+
+  const [data, setData] = useState(false);
+
+  useEffect(() => {
+    return !data ? fetchStarbucksData() : null;
+  }, [data]);
+
+  const fetchStarbucksData = () => {
+    let urlWithString = `https://raw.githubusercontent.com/aspencapital/candidate-project-ui-ux/master/data/coordinates.geojson`;
+
+    return fetch(urlWithString)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`Error code: ${res.status}. Please try again.`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  if (!data) return <section>Loading...</section>;
+
   return (
-    <div>
-      <h1>Time 4 Topology</h1>
+    <section>
+      <header>Hello!</header>
+      <button
+        onClick={() => {
+          console.log(data);
+        }}
+      >
+        Time 4 Topology
+      </button>
 
       <div>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+        <MapContainer center={position} zoom={13} scrollWheelZoom>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {data.features.map((ele) => {
+            console.log("ele", ele);
+
+            let newEle = [ele.geometry.coordinates];
+            return (
+              <Marker
+                key={ele.geometry.coordinates[1]}
+                position={[
+                  ele.geometry.coordinates[1],
+                  ele.geometry.coordinates[0],
+                ]}
+              ></Marker>
+            );
+          })}
+          <Marker position={position}>
+            <Popup>
+              Apparently. <br /> I can customize this.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
-    </div>
+    </section>
   );
 }
 
