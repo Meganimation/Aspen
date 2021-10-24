dac146b4ea78d411804a4b4b0694f145abd3651c
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2021-10-24 13:35:50 -0400

    added menu with logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 const MainWrapper = styled.main`
   display: flex;
@@ -19,8 +19,8 @@ const MapWrapper = styled.section`
 `;
 
 const StyledNav = styled.nav`
+  z-index: 2;
   background-color: rgba(0, 0, 0, 0.5);
-
   transition: 0.5s;
   position: absolute;
   height: 100%;
@@ -29,12 +29,75 @@ const StyledNav = styled.nav`
   opacity: ${(props) => (props.menu ? "0%" : "100%")};
 `;
 
+const loaderAnimation = keyframes`
+ 0% {
+  transform: rotate(0deg);
+  border: 36px solid #f3f3f3; 
+  border-top: 36px solid darkGray;
+ }
+  100% {
+    border: 6px solid #f3f3f3; 
+    border-top: 6px solid darkGray; 
+    transform: rotate(360deg);
+  }
+}
+`;
+
+const loaderAnimationRule = css`
+  ${loaderAnimation} 1.5s infinite alternate;
+`;
+
+const Loader = styled.div`
+  border: 6px solid #f3f3f3;
+  border-top: 6px solid lightGray;
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+  animation: ${loaderAnimationRule};
+`;
+
+function ErrorMessage({ error }) {
+  return (
+    <section
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          height: "50px",
+          width: "50px",
+          background: "red",
+          borderRadius: "100px",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "50px",
+            textAlign: "center",
+            color: "white",
+            margin: "auto",
+          }}
+        >
+          !
+        </h1>
+      </div>
+      <h3 style={{ position: "inherit", marginTop: "60px" }}>
+        {" "}
+        {error.toString()}{" "}
+      </h3>
+    </section>
+  );
+}
+
 function App() {
   const position = [45.514242, -122.683175];
 
   const [data, setData] = useState(false);
   const [filteredData, setFilteredData] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("Cornell Rd");
+  const [searchTerm, setSearchTerm] = useState("");
   const [menu, setMenu] = useState(true);
 
   useEffect(() => {
@@ -57,10 +120,12 @@ function App() {
       })
       .catch((err) => {
         alert(err.message);
+        setData(["error", err.message]);
       });
   };
 
-  if (!data) return <section>Loading...</section>;
+  if (!data) return <Loader />;
+  if (data[0] === "error") return <ErrorMessage error={data[1]} />;
 
   const handleFilter = () => {
     console.log("filtering", searchTerm);
@@ -101,6 +166,7 @@ function App() {
         </StyledButton>
 
         <input
+          placeholder={"eg. Cornell Road"}
           value={searchTerm}
           onChange={(e) => {
             setSearchTerm(e.target.value);
@@ -112,6 +178,16 @@ function App() {
           }}
         >
           search for a starbucks on {searchTerm}
+        </StyledButton>
+        <StyledButton
+          onMouseDown={() => {
+            setSearchTerm("All");
+          }}
+          onMouseUp={() => {
+            handleFilter();
+          }}
+        >
+          Search for all in your area
         </StyledButton>
       </StyledNav>
       {/* </div> */}
